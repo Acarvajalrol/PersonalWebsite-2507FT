f2be8b48c0ee2a511915db8a9f12deeff7137389
Alvaro Carvajal Roldan <you@example.comacarvajalrol@hotmail.com>
2025-07-24 19:24:21 -0700

    4 new html files and 1 css

---
+++ index.js
@@ -0,0 +1,18 @@
+const firstName = "Alvaro ";
+const lastName = "Carvajal";
+
+console.log(firstName, lastName);
+document.write(firstName, lastName);
+
+// const abt1 = document.getElementById("abt1");
+// console.log(abt1);
+// abt1.textContent = firstName + lastName;
+// abt1.textContent += firstName + lastName;
+
+// const element = document.createElement("h1");
+// element.textContent = "Java Trial";
+// const main = document.getElementsByClassName("mainAbout")[0];
+// console.log(main);
+// const abtdiv = document.getElementsByClassName("abtdiv")[1];
+// console.log(abtdiv);
+// main.append(element);
